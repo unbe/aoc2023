7abb272d04915ef64099fdf969b343d5bff0ce3b
artem <unbe@users.noreply.github.com>
2023-12-13 00:33:33 +0100

    day12-wip

---
+++ aoc12.py
@@ -0,0 +1,46 @@
+import sys
+import re
+import functools
+
+recs = []
+for line in sys.stdin:
+    rec, grp = line.strip().split(' ')
+    grp = list(map(int, grp.split(',')))
+    recs.append((tuple(rec), tuple(grp)))
+
+@functools.cache
+def solve(rec, grp):
+    cnt = 0
+    if len(grp) == 0:
+        cnt = +('#' not in rec)
+    if len(grp) > 0 and len(rec) > 0:
+        g = grp[0]
+        for i in range(len(rec)-g+1):
+            prefits = sum(x != '#' for x in rec[:i]) == i
+            fits = sum(x != '.' for x in rec[i:i+g]) == g
+            if prefits and fits and (i+g == len(rec) or rec[i+g] != '#'):
+                cnt += solve(rec[i+g+1:], grp[1:])
+
+    return cnt
+
+total = 0
+for r in recs:
+    s = solve(*r)
+    total += s
+print("part1:", total)
+
+def unfold(t, s):
+    t = ((list(t)) + s) * 5
+    if len(s) > 0:
+        t = t[:-len(s)]
+    return tuple(t)
+
+recs2 = []
+for r in recs:
+    recs2.append((unfold(r[0], ['?']), unfold(r[1], [])))
+    
+total = 0
+for r in recs2:
+    s = solve(*r)
+    total += s
+print("part2:", total)
