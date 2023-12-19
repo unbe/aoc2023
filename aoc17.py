73d90804cd1c1ce294be5e2abb6e0aa67ad8fa5c
unbeli <unbe@unbe.li>
2023-12-19 21:53:13 +0100

    day17

--- aoc17.py
+++ aoc17.py
@@ -28,40 +28,17 @@ def solve(d_min, d_max):
     dist = {}
     dist[s] = 0
     pq = [(0, s)]
-    prev = {}
 
     while len(pq) > 0:
         _, u = heapq.heappop(pq)
         for v, l in moves(u, d_min, d_max):
             if v not in dist or dist[u] + l < dist[v]:
                 dist[v] = dist[u] + l
-                prev[v] = u
                 heapq.heappush(pq, (dist[v], v))
 
     stop = (len(maze)-1, len(maze[0])-1)
-    end, best_v = sorted([(k, v) for k, v in dist.items() if (k[0], k[1]) == stop and k[2] >= d_min and k[2] <= d_max], key=lambda u: u[1])[0]
-    return (best_v, end, prev)   # extra values for debugging
+    valid = [(k, v) for k, v in dist.items() if (k[0], k[1]) == stop and k[2] >= d_min]
+    return sorted(valid, key=lambda u: u[1])[0][1]
 
-print('part1:', solve(0, 3)[0])
-print('part2:', solve(4, 10)[0])
-
-
-"""
-path = {}
-path[(r[0], r[1])] = '*'
-sm = maze[r[0]][r[1]]
-while r in prev:
-    r = prev[r]
-    path[(r[0], r[1])] = '*'
-    sm += maze[r[0]][r[1]]
-    # print(r, dist[r], maze[r[0]][r[1]], sm)
-
-for x, r in enumerate(maze):
-    for y, n in enumerate(r):
-        if (x, y) in path:
-            print('\033[1m', end='')
-        print(maze[x][y], end='')
-        if (x, y) in path:
-            print('\033[0m', end='')
-    print('')
-"""
+print('part1:', solve(0, 3))
+print('part2:', solve(4, 10))
