7c719018354761fc8831cebe072e06c56a923617
unbe <unbe@users.noreply.github.com>
2024-01-30 21:21:48 +0100

    day25

--- aoc25.py
+++ aoc25.py
@@ -4,14 +4,12 @@ import operator
 import networkx as nx
 from networkx.algorithms.connectivity import minimum_edge_cut
 
-E = []
+G=nx.Graph()
 for line in sys.stdin:
   a, b = line.strip().split(': ')
   b = b.split(' ')
-  E.extend(zip([a]*len(b), b))
+  G.add_edges_from(zip([a]*len(b), b))
 
-G=nx.Graph()
-G.add_edges_from(E)
 mec = nx.minimum_edge_cut(G)
 G.remove_edges_from(mec)
 cc = list(nx.connected_components(G))
